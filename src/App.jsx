f6c7b4be242fb0808118e23632f1d00b21c128f4
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-10-02 00:41:19 -0500

    fix: redirects old link to new link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Hero from './Hero.jsx'
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import {Route, Routes, BrowserRouter, Redirect, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div>
         <BrowserRouter>
         <Routes>
-            <Route path="/" component={<Hero/>} />
-            <Route path="/danielkim.github.io/" component={<Hero/>} />
+        <Route exact path="/danielkim.github.io">
+          <Redirect to="/" />
+        </Route>
+        <Route exact path="/">
+            <Hero />
+        </Route>
+          {/* Your homepage component */}
         </Routes>
         </BrowserRouter>
     </div>
